11ca216534f9b42236ebc91f9257697bc68f2497
phamngocthai <ngocthaipham99@gmail.com>
2021-10-06 20:16:32 +0700

    commit

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ var path = require("path");
 
 app.use(cookieParser());
 // app.use(cors());
-app.use(
-  cors({ credential: true, origin: "https://deploy-easy-english.herokuapp.com" })
-);
+app.use(cors({ credential: true, origin: `http://localhost:${port}` }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var db = mysql.createConnection({
